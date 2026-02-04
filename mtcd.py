0d8555f74c4ae7ad724ce18982104b62706f07cc
Ludovic Cellard <ludovic.cellard@free.fr>
2026-02-04 16:20:08 +0100

    Update mtcd.py

--- mtcd.py
+++ mtcd.py
@@ -1,15 +1,20 @@
 #initialisation des commandes, ....
 #instanciation et initialisation
+consigne_temp_nini=13
+consigne_temp_nini=16
 
 #fonctions_pilotées
-
+def reglage_temp_nini(consigne_temp_nini)
+    if temp<consigne_temp_nini
+        pilote_chauffage.on()
 
 #######################################################        
 def traitement_commandes_dynamiques (commande):
     try :
         commande_split=commande.split('=')
         ##############################################
-        
+        if commande_split[0] == 'temp_nini':
+            reglage_temp_nini(int(commande_split[1]))
         
         
         
@@ -17,4 +22,5 @@ def traitement_commandes_dynamiques (commande):
     except :
         pass
     
+
     
