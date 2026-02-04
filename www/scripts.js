b34d24a220d60d5a3492653ba86a80da8595fee6
Ludovic Cellard <ludovic.cellard@free.fr>
2026-02-04 16:06:16 +0100

    Update scripts.js

--- www/scripts.js
+++ www/scripts.js
@@ -18,4 +18,19 @@ function fct_humid_max() {
 	commande_XHR.open("GET","/?humid_max"+document.getElementById("humid_max").value,true);
 	commande_XHR.send()
 }
-
+function initialisation_variable() {
+	var commande_XHR = XMLHttpRequest();
+	let temp_nini_value = document.getElementById("temp_nini").value;
+	let temp_max_value = document.getElementById("temp_max").value;
+	let humid_mini_value = document.getElementById("humid_mini").value;
+	let humid_max_value = document.getElementById("humid_max").value;
+	commande_XHR.open("GET","/?temp_nini"+temp_nini_value,true);
+	commande_XHR.send();
+	commande_XHR.open("GET","/?temp_max"+temp_max_value,true);
+	commande_XHR.send();
+	commande_XHR.open("GET","/?humid_mini"+humid_mini_value,true);
+	commande_XHR.send();
+	commande_XHR.open("GET","/?humid_max"+temp_nini_value,true);
+	commande_XHR.send();
+};
+initialisation_variable()
