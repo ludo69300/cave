5236a07f92aa54002e5ecc4d2fd87e1cdf49ab3a
Ludovic Cellard <ludovic.cellard@free.fr>
2026-02-04 15:36:53 +0100

    Add functions for temperature and humidity requests

--- www/scripts.js
+++ www/scripts.js
@@ -1,1 +1,21 @@
+function fct_temp_nini() {
+	var commande_XHR = XMLHttpRequest();
+	commande_XHR.open("GET","/?temp_nini"+document.getElementById("temp_nini").value,true);
+	commande_XHR.send()
+}
+function fct_temp_max() {
+	var commande_XHR = XMLHttpRequest();
+	commande_XHR.open("GET","/?temp_max("+document.getElementById("temp_max").value,true);
+	commande_XHR.send()
+}
+function fct_humid_mini() {
+	var commande_XHR = XMLHttpRequest();
+	commande_XHR.open("GET","/?humid_mini"+document.getElementById("humid_mini").value,true);
+	commande_XHR.send()
+}
+function fct_humid_max() {
+	var commande_XHR = XMLHttpRequest();
+	commande_XHR.open("GET","/?humid_max"+document.getElementById("humid_max").value,true);
+	commande_XHR.send()
+}
 
